b9eff464fcd8899862ed6731c80d41afcb1cc7b0
İrfan Sütlü <peroxcid@haotmail.com>
2022-09-01 22:23:35 +0300

    check-form

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const incr1 = document.getElementById("incr1");
 const decr1 = document.getElementById("decr1");
 const counter1 = document.getElementById("counter1");
 
+const incr2 = document.getElementById("incr2");
+const decr2 = document.getElementById("decr2");
+const counter2 = document.getElementById("counter2");
+
 let totalCount1 = 0;
+
+let totalCount2 = 0;
 
 incr1.addEventListener("click", () => {
   totalCount1++;
@@ -14,30 +20,30 @@ incr1.addEventListener("click", () => {
 decr1.addEventListener("click", () => {
   if (totalCount1 > 0) {
     totalCount1--;
+    total.innerHTML ="$" + (totalCount1 * 54.99 + totalCount2 * 74.99 + 19).toFixed(2);
     uptadeCounter1();
   }
 });
-function uptadeCounter1() {
-  counter1.innerHTML = totalCount1;
-}
-
-const incr2 = document.getElementById("incr2");
-const decr2 = document.getElementById("decr2");
-const counter2 = document.getElementById("counter2");
-
-let totalCount2 = 0;
 
 incr2.addEventListener("click", () => {
   totalCount2++;
+  total.innerHTML ="$" + (totalCount1 * 54.99 + totalCount2 * 74.99 + 19).toFixed(2);
   uptadeCounter2();
 });
 
 decr2.addEventListener("click", () => {
   if (totalCount2 > 0) {
     totalCount2--;
+    total.innerHTML ="$" + (totalCount1 * 54.99 + totalCount2 * 74.99 + 19).toFixed(2);
     uptadeCounter2();
   }
 });
+
+
+function uptadeCounter1() {
+  counter1.innerHTML = totalCount1;
+}
+
 function uptadeCounter2() {
   counter2.innerHTML = totalCount2;
 }
